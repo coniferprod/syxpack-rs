47ad6c194cbe4497e88b6a4a654b266fe3a1a6a2
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-04-08 08:54:00 +0300

    Better file read

--- src/bin/syxident.rs
+++ src/bin/syxident.rs
@@ -3,11 +3,20 @@ use std::fs;
 use std::env;
 use syxpack::{Message, UniversalKind, message_count, split_messages};
 
-fn read_file(name: &String) -> Vec<u8> {
-    let mut f = fs::File::open(&name).expect("no file found");
-    let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer).expect("unable to read file");
-    buffer
+fn read_file(name: &String) -> Option<Vec<u8>> {
+    match fs::File::open(&name) {
+        Ok(mut f) => {
+            let mut buffer = Vec::new();
+            match f.read_to_end(&mut buffer) {
+                Ok(_) => Some(buffer),
+                Err(_) => None
+            }
+        },
+        Err(_) => {
+            eprintln!("Unable to open file {}", name);
+            None
+        }
+    }
 }
 
 fn main() {
@@ -18,29 +27,30 @@ fn main() {
     }
 
     let input_file = &args[1];
-    let buffer = read_file(input_file);
 
-    let mut all_messages: Vec<Message> = Vec::new();
-    let count = message_count(buffer.to_vec());
-    if count >= 1 {
-        if count == 1 {
-            all_messages.push(Message::new(buffer.to_vec()).ok().unwrap());
-        }
-        else {
-            let messages = split_messages(buffer.to_vec());
-            for message in messages {
-                all_messages.push(Message::new(message).ok().unwrap());
+    if let Some(buffer) = read_file(input_file) {
+        let mut all_messages: Vec<Message> = Vec::new();
+        let count = message_count(buffer.to_vec());
+        if count >= 1 {
+            if count == 1 {
+                all_messages.push(Message::new(buffer.to_vec()).ok().unwrap());
             }
-        }
-    };
+            else {
+                let messages = split_messages(buffer.to_vec());
+                for message in messages {
+                    all_messages.push(Message::new(message).ok().unwrap());
+                }
+            }
+        };
 
-    let mut number = 1;
-    for message in all_messages {
-        println!("Message {} of {}", number, count);
-        identify(&message);
-        println!("MD5 digest: {:x}", message.digest());
-        println!();
-        number += 1;
+        let mut number = 1;
+        for message in all_messages {
+            println!("Message {} of {}", number, count);
+            identify(&message);
+            println!("MD5 digest: {:x}", message.digest());
+            println!();
+            number += 1;
+        }
     }
 }
 
