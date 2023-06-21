b94ffb2c86ae36f3d26858a3d57ab2d4081763f9
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-06-21 14:48:24 +0300

    Save the message from syxreceive to a disk file

--- src/bin/syxreceive.rs
+++ src/bin/syxreceive.rs
@@ -1,57 +1,73 @@
 // Reads a line of input produced by ReceiveMIDI
-// filters out everything else but MIDI System Exclusive messages,
+// Filters out everything else but MIDI System Exclusive messages,
 // and interprets the message data.
 
-use std::io::prelude::*;
-use std::fs;
-use std::env;
-
-use syxpack::{Message, message_count, read_file};
+use std::io::Write;
+use std::fs::File;
+use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {  // read from standard input
-        loop {
-            let mut input = String::new();
-            match std::io::stdin().read_line(&mut input) {
-                Ok(len) => if len == 0 {
-                    return;
+    loop {
+        let mut input = String::new();
+        match std::io::stdin().read_line(&mut input) {
+            Ok(len) => if len == 0 {
+                return;
+            }
+            else {
+                let parts: Vec<&str> = input.split_whitespace().collect();
+
+                // We want at least "system-exclusive", "hex" or "dec", and one byte
+                if parts.len() < 3 {
+                    continue;
                 }
-                else {
-                    process_line(&input);
-                },
-                Err(e) => {
-                    eprintln!("{}", e);
-                    std::process::exit(1);
+
+                // Only deal with SysEx:
+                if parts[0] == "system-exclusive" {
+                    // Get the base of the byte strings.
+                    let base = if parts[1] == "hex" { 16 } else { 10 };
+
+                    let mut data: Vec<u8> = Vec::new();
+
+                    for part in &parts[2..] {
+                        match u8::from_str_radix(part, base) {
+                            Ok(b) => data.push(b),
+                            Err(_) => {
+                                //eprintln!("Error in byte string '{}': {}", part, e);
+                                continue;
+                            }
+                        }
+                    }
+
+                    // Add the MIDI System Exclusive delimiters:
+                    data.insert(0, 0xf0);
+                    data.push(0xf7);
+
+                    println!("Received {} bytes of System Exclusive data", data.len());
+
+                    // Write the data into a file named by the current timestamp.
+                    let now = SystemTime::now();
+                    let epoch_now = now
+                        .duration_since(UNIX_EPOCH)
+                        .expect("System time should be after Unix epoch");
+                    let filename = format!("{:?}.syx", epoch_now.as_secs());
+                    let path = Path::new(&filename);
+                    let display = path.display();
+                    let mut file = match File::create(&path) {
+                        Err(why) => panic!("couldn't create {}: {}", display, why),
+                        Ok(file) => file,
+                    };
+
+                    match file.write_all(&data) {
+                        Err(why) => panic!("couldn't write to {}: {}", display, why),
+                        Ok(_) => { },
+                    }
                 }
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1);
             }
         }
-    }
-    else if args.len() == 2 {  // read from input file
-        let input_file = &args[1];
-
-    }
-    else {
-        println!("usage: syxreceive [infile]");
-        std::process::exit(1);
-    }
-}
-
-fn process_line(line: &str) {
-    let parts: Vec<&str> = line.split_whitespace().collect();
-    if parts[0] == "system-exclusive" {
-        let mut data: Vec<u8> = Vec::new();
-
-        for part in &parts[1..] {
-            match u8::from_str_radix(part, 16) {
-                Ok(b) => data.push(b),
-                Err(e) => {
-                    eprintln!("Error in hex string: {}", e);
-                    return;
-                }
-            }
-        }
-
-        println!("data = {:?}", data);
     }
 }
